b0a1a1ea83cdb4543bb0b000843b03dcbfe0d4f0
sk <content.type.text.net@gmail.com>
2021-12-03 22:52:39 +0900

    DIコンテナ無いのに何を頑張ろうとしているのか・・・

--- public_html/PeServer/Core/ILogger.php
+++ public_html/PeServer/Core/ILogger.php
@@ -6,4 +6,15 @@ namespace PeServer\Core;
 
 interface ILogger
 {
+	const LEVEL_TRACE = 1;
+	const LEVEL_DEBUG = 2;
+	const LEVEL_INFO = 3;
+	const LEVEL_WARN = 4;
+	const LEVEL_ERROR = 5;
+
+	function trace(string $message, ?array $parameters = null): void;
+	function debug(string $message, ?array $parameters = null): void;
+	function info(string $message, ?array $parameters = null): void;
+	function warn(string $message, ?array $parameters = null): void;
+	function error(string $message, ?array $parameters = null): void;
 }
--- public_html/PeServer/Core/Logger.php
+++ public_html/PeServer/Core/Logger.php
@@ -8,10 +8,42 @@ require_once('PeServer/Core/ILogger.php');
 
 class Logger implements ILogger
 {
-	private $header;
+	protected $traceIndex = 2;
 
-	public function __construct(string $header)
+	protected $header;
+	protected $loggingConfiguration;
+
+	public function __construct(string $header, array $loggingConfiguration)
 	{
 		$this->header = $header;
+		$this->loggingConfiguration = $loggingConfiguration;
+	}
+
+	private function log(int $level, int $traceIndex, string $message, ?array $parameters = null)
+	{
+		if($this->loggingConfiguration['level'] < $level) {
+			return;
+		}
+	}
+
+	public function trace(string $message, ?array $parameters = null): void
+	{
+		$this->log(self::LEVEL_TRACE, $this->traceIndex, $message, $parameters);
+	}
+	public function debug(string $message, ?array $parameters = null): void
+	{
+		$this->log(self::LEVEL_DEBUG, $this->traceIndex, $message, $parameters);
+	}
+	public function info(string $message, ?array $parameters = null): void
+	{
+		$this->log(self::LEVEL_INFO, $this->traceIndex, $message, $parameters);
+	}
+	public function warn(string $message, ?array $parameters = null): void
+	{
+		$this->log(self::LEVEL_WARN, $this->traceIndex, $message, $parameters);
+	}
+	public function error(string $message, ?array $parameters = null): void
+	{
+		$this->log(self::LEVEL_ERROR, $this->traceIndex, $message, $parameters);
 	}
 }
