a3580bd276af3508ad0a93f2816931beb229e078
sk <content.type.text.net@gmail.com>
2021-12-09 22:09:27 +0900

    ちゃんとPHP勉強せなこれは無理だ

--- public_html/php-deploy-receiver/script.php
+++ public_html/php-deploy-receiver/script.php
@@ -1,23 +1,18 @@
 <?php
 
-declare(strict_types=1);
-
-require __DIR__ . '/php-deploy-receiver.php';
-
-
-function before_update(ScriptArgument $scriptArgument)
+function before_update($scriptArgument)
 {
-	$scriptArgument->log('before script');
+	// $scriptArgument->log('before script');
 
-	//TODO: キャッシュの破棄
-	$removeDirs = [
-		$scriptArgument->joinPath($scriptArgument->publicDirectoryPath, 'data', 'temp', 'views', 'c'),
-	];
+	// //TODO: キャッシュの破棄
+	// $removeDirs = [
+	// 	$scriptArgument->joinPath($scriptArgument->publicDirectoryPath, 'data', 'temp', 'views', 'c'),
+	// ];
 }
 
 
-function after_update(ScriptArgument $scriptArgument)
+function after_update($scriptArgument)
 {
-	$scriptArgument->log('after script');
-	//TODO: DBのマイグレーション処理
+	// $scriptArgument->log('after script');
+	// //TODO: DBのマイグレーション処理
 }
