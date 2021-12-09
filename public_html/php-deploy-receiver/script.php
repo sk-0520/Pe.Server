cf4d1589389e0de0980afd09e830dd56e277247f
sk <content.type.text.net@gmail.com>
2021-12-09 21:19:34 +0900

    後で頑張るわ

--- public_html/php-deploy-receiver/script.php
+++ public_html/php-deploy-receiver/script.php
@@ -2,13 +2,24 @@
 
 declare(strict_types=1);
 
+function joinPath(string $basePath, string ...$addPaths): string
+{
+	$paths = array_merge([$basePath], $addPaths);
+
+	$joinedPath = implode(DIRECTORY_SEPARATOR, $paths);
+	return $joinedPath;
+}
+
 function before_update(array $scriptData)
 {
-	echo 'before_update';
+	//TODO: キャッシュの破棄
+	$removeDirs = [
+		joinPath($scriptData['public'], 'data', 'temp', 'views', 'c'),
+	];
 }
 
 
 function after_update(array $scriptData)
 {
-	echo 'after_update';
+	//TODO: DBのマイグレーション処理
 }
