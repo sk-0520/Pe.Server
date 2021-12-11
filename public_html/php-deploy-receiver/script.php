755478be293af6247ba8134e9a9c01803d4ebc31
sk <content.type.text.net@gmail.com>
2021-12-11 11:20:17 +0900

    update

--- public_html/php-deploy-receiver/script.php
+++ public_html/php-deploy-receiver/script.php
@@ -1,18 +1,47 @@
 <?php
 
-function before_update($scriptArgument)
+class Deploy
 {
-	$scriptArgument->log('before script');
+	private $scriptArgument;
 
-	// //TODO: キャッシュの破棄
-	// $removeDirs = [
-	// 	$scriptArgument->joinPath($scriptArgument->publicDirectoryPath, 'data', 'temp', 'views', 'c'),
-	// ];
+	public function __construct(ScriptArgument $scriptArgument)
+	{
+		$this->scriptArgument = $scriptArgument;
+	}
+
+	public function before(): void
+	{
+		$this->scriptArgument->log('before script');
+
+		// キャッシュの破棄
+		$removeDirs = [
+			$this->scriptArgument->joinPath($this->scriptArgument->publicDirectoryPath, 'data', 'temp', 'views', 'c'),
+		];
+
+		foreach ($removeDirs as $removeDir) {
+			$this->scriptArgument->log('remove: ' . $removeDir);
+			$this->scriptArgument->cleanupDirectory($removeDir);
+		}
+
+		// DBバックアップ
+	}
+
+	public function after(): void
+	{
+		$this->scriptArgument->log('after script');
+		//TODO: DBのマイグレーション処理
+	}
+}
+
+function before_update(ScriptArgument $scriptArgument)
+{
+	$deploy = new Deploy($scriptArgument);
+	$deploy->before();
 }
 
 
-function after_update($scriptArgument)
+function after_update(ScriptArgument $scriptArgument)
 {
-	$scriptArgument->log('after script');
-	// //TODO: DBのマイグレーション処理
+	$deploy = new Deploy($scriptArgument);
+	$deploy->after();
 }
