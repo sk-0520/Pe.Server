857a2dc754f15150459d81cbaed24431e104fe4d
sk <content.type.text.net@gmail.com>
2021-12-13 19:06:26 +0900

    phpunitの展開処理を追加しておいた
    コード補完してくれないときついのだ

---
+++ test/oreoreextract.bat
@@ -0,0 +1,7 @@
+echo off
+cd /d %~dp0
+
+set PATH=C:\Applications\xampp\xampp-portable-win32-7.1.1-0-VC14\xampp\php;%PATH%
+
+php -r "$phar = new Phar('phpunit.phar'); $phar->extractTo('./PHPUnit', null, true);"
+
