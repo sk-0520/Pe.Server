6ba03e95f60f4324f254fa6d9a1e839a322396c3
sk <content.type.text.net@gmail.com>
2021-12-14 23:57:05 +0900

    環境設定を外部化

--- dev/oreore/extract.bat
+++ dev/oreore/extract.bat
@@ -1,7 +1,7 @@
 echo off
 cd /d %~dp0
 
-set PATH=C:\Applications\xampp\xampp-portable-win32-7.1.1-0-VC14\xampp\php;%PATH%
+call env.bat
 
 php -r "$phar = new Phar('..\\..\\test\\phpunit.phar'); $phar->extractTo('..\\PHPUnit', null, true);"
 
--- dev/oreore/test.bat
+++ dev/oreore/test.bat
@@ -2,7 +2,6 @@ echo off
 cd /d %~dp0
 
 rem wget -O phpunit https://phar.phpunit.de/phpunit-7.phar
-set PATH=C:\Applications\xampp\xampp-portable-win32-7.1.1-0-VC14\xampp\php;%PATH%
-set BASH=C:\Program Files\Git\bin\bash.exe
+call env.bat
 
 "%BASH%" ..\test.sh
