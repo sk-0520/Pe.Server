904f49fb5642d51d6b12d84c617bce4fd1202d16
sk <content.type.text.net@gmail.com>
2021-12-15 21:34:17 +0900

    やっぱlinqすげーわ

---
+++ public_html/PeServer/Core/Collection.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PeServer\Core;
+
+use \Traversable;
+use \ArrayIterator;
+use \IteratorAggregate;
+
+/**
+ * LINQ 的なことがしたいけど PHP のイテレータ処理知らんからとりあえず適当実装。
+ */
+class Collection implements IteratorAggregate // @phpstan-ignore-line
+{
+	/** @var array */
+	private $items; // @phpstan-ignore-line
+
+	private function __construct(array $items) // @phpstan-ignore-line
+	{
+		$this->items = $items;
+	}
+
+	/**
+	 * Undocumented function
+	 *
+	 * @return Traversable
+	 */
+	public function getIterator(): Traversable // @phpstan-ignore-line
+	{
+		return new ArrayIterator($this->items);
+	}
+
+	public function toArray()
+	{
+		return array_values($this->items);
+	}
+
+	/**
+	 * 開始。
+	 *
+	 * new でメソッドをつなげられん。
+	 *
+	 * @param array $items
+	 * @return Collection
+	 */
+	public static function from(array $items): Collection // @phpstan-ignore-line
+	{
+		return new Collection($items);
+	}
+
+	// ----------------------------------------------------
+
+	/**
+	 * Undocumented function
+	 *
+	 * @param callable $callback
+	 * @return Collection
+	 */
+	function where(callable $callback): Collection // @phpstan-ignore-line
+	{
+		return self::from(array_filter($this->items, $callback));
+	}
+}
---
+++ test/PeServerTest/Core/CollectionTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PeServerTest\Core;
+
+use \PeServerTest\Data;
+use \PeServerTest\TestClass;
+use \PeServer\Core\Collection;
+
+class CollectionTest extends TestClass
+{
+	static $array = [
+		1, 2, 3, 4, 5, 6
+	];
+
+	public function test_where()
+	{
+		$expected1 = [2, 4, 6];
+		$actual1 = Collection::from(self::$array)
+			->where(function($i) { return $i % 2 == 0; })
+			->toArray()
+		;
+		$this->assertSame($expected1, $actual1);
+	}
+}
