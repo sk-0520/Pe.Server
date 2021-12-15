9224268e88be716d24c42fb8b232eb798a6cbd07
sk <content.type.text.net@gmail.com>
2021-12-15 10:43:13 +0900

    ファイル行方不明なう

--- dev/send.sh
+++ dev/send.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ue
 
-#export SETTING_URL=http://peserver.starfree.jp/deploy/php-deploy-receiver.php
-export SETTING_URL=http://peserver.php.xdomain.jp/deploy/php-deploy-receiver.php
+export SETTING_URL=http://peserver.starfree.jp/deploy/php-deploy-receiver.php
+#export SETTING_URL=http://peserver.php.xdomain.jp/deploy/php-deploy-receiver.php
 export SETTING_AUTH_HEADER_NAME=DEPLOY
 export SETTING_ARCHIVE_FILE_NAME=public_html.zip
 export SETTING_SPLIT_SIZE=10MB
