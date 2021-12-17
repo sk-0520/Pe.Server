100a501c463c0cdf85eea1a302f6650f56603240
sk <content.type.text.net@gmail.com>
2021-12-17 20:50:03 +0900

    マイグレーション対応

--- public_html/deploy/script.php
+++ public_html/deploy/script.php
@@ -32,7 +32,7 @@ class DeployScript
 
 		$backupDirectoryPath = $this->scriptArgument->joinPath($this->getAppDirectoryPath(), 'data', 'backups');
 		if (!file_exists($backupDirectoryPath)) {
-			mkdir($backupDirectoryPath, 077, true);
+			mkdir($backupDirectoryPath, 0777, true);
 		}
 		$backupArchiveFilePath = $this->scriptArgument->joinPath($backupDirectoryPath, $this->fileTimestampName . '.zip');
 
@@ -86,7 +86,7 @@ class DeployScript
 
 		// SQLite を使うのは決定事項である！
 		$filePath = $databaseSetting['connection'];
-		$dbVersion = 0;
+		$dbVersion = -1;
 		if (file_exists($filePath)) {
 			$checkPdo = $this->createConnection($databaseSetting);
 			$checkCountStatement = $checkPdo->query("select COUNT(*) from sqlite_master where sqlite_master.type='table' and sqlite_master.name='database_version'");
@@ -94,7 +94,7 @@ class DeployScript
 				$versionStatement = $checkPdo->query("select version from database_version");
 				$versionNumber = $versionStatement->fetchColumn();
 				if ($versionNumber !== false) {
-					$dbVersion = $versionNumber;
+					$dbVersion = (int)$versionNumber;
 				}
 				$versionStatement = null;
 			}
@@ -103,20 +103,25 @@ class DeployScript
 		}
 
 		$db_migrates = [
-			'db_migrates_0',
+			'db_migrates_0000',
 		];
 
+		$newVersion = 0;
 		$pdo = $this->createConnection($databaseSetting);
-		$pdo->exec( 'PRAGMA foreign_keys = OFF;' );
-		for ($i = $dbVersion; $i < count($db_migrates); $i++) {
+		$pdo->exec('PRAGMA foreign_keys = OFF;');
+		for ($i = $dbVersion + 1; $i < count($db_migrates); $i++) {
 			$this->scriptArgument->log('DB: ' . $db_migrates[$i]);
 			call_user_func(array($this, $db_migrates[$i]), $pdo);
+			$newVersion = $i;
 		}
-		$pdo->exec( 'PRAGMA foreign_keys = ON;' );
+
+		$this->db_migrates_9999($pdo, $dbVersion, $newVersion);
+
+		$pdo->exec('PRAGMA foreign_keys = ON;');
 	}
 
 	// 管理ユーザー admin/admin が作られるので公開時は速やかに破棄 or 変更すること
-	private function db_migrates_0(PDO $pdo)
+	private function db_migrates_0000(PDO $pdo)
 	{
 		//TODO: 全削除処理
 		$tablesStatement = $pdo->query("select sqlite_master.name from sqlite_master where sqlite_master.type='table' and sqlite_master.name <> 'sqlite_sequence'");
@@ -132,99 +137,112 @@ class DeployScript
 		//TODO: 暗号化とかとか
 		$userId = '00000000-0000-4000-0000-000000000000';
 		$loginId = 'init';
-		$password = 'init';
+		$password = bin2hex(openssl_random_pseudo_bytes(4));
 
 		$pdo->exec(
 			<<<SQL
-			create table
-				[database_version]
-				(
-					[version] integer not null
-				)
-			;
+create table
+	[database_version]
+	(
+		[version] integer not null
+	)
+;
 
-			create table
-				[users]
-				(
-					[user_id] text not null,
-					[login_id] text not null unique,
-					[level] text not null,
-					[state] text not null,
-					[name] text not null,
-					[mail_address] text not null,
-					[website] text not null,
-					[note] text not null,
-					primary key([user_id])
-				)
-			;
+create table
+	[users]
+	(
+		[user_id] text not null,
+		[login_id] text not null unique,
+		[level] text not null,
+		[state] text not null,
+		[name] text not null,
+		[mail_address] text not null,
+		[website] text not null,
+		[note] text not null,
+		primary key([user_id])
+	)
+;
 
-			create table
-				[user_authentications]
-				(
-					[user_id] text not null,
-					[default_password] text not null,
-					[current_password] text not null,
-					primary key([user_id]),
-					foreign key ([user_id]) references users([user_id])
-				)
-			;
+create table
+	[user_authentications]
+	(
+		[user_id] text not null,
+		[default_password] text not null,
+		[current_password] text not null,
+		primary key([user_id]),
+		foreign key ([user_id]) references users([user_id])
+	)
+;
 
-			create table
-				[user_audit_logs]
-				(
-					[sequence] integer not null,
-					[user_id] text not null,
-					[timestamp] datetime not null,
-					[event] text not null,
-					[ip_address] text not null,
-					[user_agent] text not null,
-					primary key([sequence] autoincrement),
-					foreign key ([user_id]) references users([user_id])
-				)
-			;
+create table
+	[user_audit_logs]
+	(
+		[sequence] integer not null,
+		[user_id] text not null,
+		[timestamp] datetime not null,
+		[event] text not null,
+		[ip_address] text not null,
+		[user_agent] text not null,
+		primary key([sequence] autoincrement),
+		foreign key ([user_id]) references users([user_id])
+	)
+;
 
-			insert into
-				[users]
-				(
-					[user_id],
-					[login_id],
-					[level],
-					[state],
-					[name],
-					[mail_address],
-					[website],
-					[note]
-				)
-				values
-				(
-					'$userId',
-					'$loginId',
-					'init',
-					'enabled',
-					'initial-user',
-					'init@localhost',
-					'http://localhost',
-					''
-				)
-			;
+insert into
+	[users]
+	(
+		[user_id],
+		[login_id],
+		[level],
+		[state],
+		[name],
+		[mail_address],
+		[website],
+		[note]
+	)
+	values
+	(
+		'$userId',
+		'$loginId',
+		'init',
+		'enabled',
+		'initial-user',
+		'init@localhost',
+		'http://localhost',
+		''
+	)
+;
 
-			insert into
-				[user_authentications]
-				(
-					[user_id],
-					[default_password],
-					[current_password]
-				)
-				values
-				(
-					'$userId',
-					'',
-					'$password'
-				)
-			;
+insert into
+	[user_authentications]
+	(
+		[user_id],
+		[default_password],
+		[current_password]
+	)
+	values
+	(
+		'$userId',
+		'',
+		'$password'
+	)
+;
 
 SQL
 		);
+	}
+
+	private function db_migrates_9999(PDO $pdo, int $oldVersion, int $newVersion)
+	{
+		if ($oldVersion === $newVersion) {
+			return;
+		}
+
+		if ($oldVersion === -1) {
+			$pdo->exec("insert into database_version(version) values (${newVersion})");
+		} else {
+			$pdo->exec("update database_version set version = ${newVersion}");
+		}
 	}
 }
 
