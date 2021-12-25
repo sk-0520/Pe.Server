e417f1f50f83fe8acdc4af13a9c7ccc0c296e0e5
sk <content.type.text.net@gmail.com>
2021-12-25 19:58:34 +0900

    ApiLogicBase

--- public_html/PeServer/App/Models/Domains/Api/ApiLogicBase.php
+++ public_html/PeServer/App/Models/Domains/Api/ApiLogicBase.php
@@ -4,15 +4,21 @@ declare(strict_types=1);
 
 namespace PeServer\App\Models\Domains\Api;
 
+use \PeServer\Core\Mvc\LogicCallMode;
 use \PeServer\Core\Mvc\LogicParameter;
 use \PeServer\App\Models\Domains\DomainLogicBase;
-use PeServer\Core\Throws\NotImplementedException;
+use \PeServer\Core\Throws\NotImplementedException;
 
 abstract class ApiLogicBase extends DomainLogicBase
 {
 	public function __construct(LogicParameter $parameter)
 	{
 		parent::__construct($parameter);
+	}
+
+	protected function registerKeysImpl(LogicCallMode $callMode): void
+	{
+		//NONE
 	}
 
 	protected function getAuditUserInfo(): array|null
