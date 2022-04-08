08c66ea0d7c6856d83ee308de3aeb01411f29cc4
sk <content.type.text.net@gmail.com>
2022-04-08 21:10:37 +0900

    ソート順対応

--- public_html/PeServer/App/Models/Domain/Page/Plugin/PluginIndexLogic.php
+++ public_html/PeServer/App/Models/Domain/Page/Plugin/PluginIndexLogic.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace PeServer\App\Models\Domain\Page\Plugin;
 
-use PeServer\App\Models\AppDatabaseCache;
 use PeServer\Core\Mvc\LogicCallMode;
 use PeServer\Core\Mvc\LogicParameter;
+use PeServer\App\Models\AppDatabaseCache;
+use PeServer\App\Models\Cache\PluginCache;
 use PeServer\App\Models\Domain\Page\PageLogicBase;
 
 class PluginIndexLogic extends PageLogicBase
@@ -24,6 +25,11 @@ class PluginIndexLogic extends PageLogicBase
 	protected function executeImpl(LogicCallMode $callMode): void
 	{
 		$pluginInformation = AppDatabaseCache::readPluginInformation();
+
+		usort($pluginInformation, function(PluginCache $a, PluginCache $b) {
+			return strcmp($a->pluginName, $b->pluginName);
+		});
+
 		$this->setValue('plugins', $pluginInformation);
 	}
 }
