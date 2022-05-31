ef3acd79d3d629f79dc2d5a608fa764ecba79154
sk <content.type.text.net@gmail.com>
2022-06-01 00:39:37 +0900

    PHPの正規表現と仲良くないんよなぁ

--- public_html/PeServer/Core/TypeConverter.php
+++ public_html/PeServer/Core/TypeConverter.php
@@ -24,7 +24,7 @@ abstract class TypeConverter
 	 */
 	public static function parseInteger(string $input): int
 	{
-		if (!preg_match(self::INT_PATTERN, $input)) {
+		if (!Regex::isMatch($input, self::INT_PATTERN)) {
 			throw new ParseException($input);
 		}
 
@@ -40,7 +40,7 @@ abstract class TypeConverter
 	 */
 	public static function tryParseInteger(string $input, ?int &$result): bool
 	{
-		if (!preg_match(self::INT_PATTERN, $input)) {
+		if (!Regex::isMatch($input, self::INT_PATTERN)) {
 			return false;
 		}
 
