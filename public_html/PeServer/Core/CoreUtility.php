907789bda3899d10e13dc81fea1986b6c01842bf
sk <content.type.text.net@gmail.com>
2022-07-26 20:05:20 +0900

    Core 内のあれこれ列挙を外へ

--- public_html/PeServer/Core/CoreUtility.php
+++ public_html/PeServer/Core/CoreUtility.php
@@ -6,18 +6,35 @@ namespace PeServer\Core;
 
 abstract class CoreUtility
 {
+	/**
+	 * ライブラリディレクトリ パス一覧 取得。
+	 *
+	 * @return string[]
+	 */
 	public static function getLibraryDirectoryParts(): array
 	{
 		return ['Core', 'Libs'];
 	}
 
+	/**
+	 * フォントディレクトリ パス一覧 取得。
+	 *
+	 * @return string[]
+	 */
 	public static function getFontDirectoryParts(): array
 	{
+		//@phpstan-ignore-next-line
 		return [...self::getLibraryDirectoryParts(), 'fonts'];
 	}
 
+	/**
+	 * 標準フォント パス一覧 取得。
+	 *
+	 * @return string[]
+	 */
 	public static function getDefaultFontParts(): array
 	{
+		//@phpstan-ignore-next-line
 		return [...self::getFontDirectoryParts(), 'migmix', 'migmix-1m-regular.ttf'];
 	}
 }
