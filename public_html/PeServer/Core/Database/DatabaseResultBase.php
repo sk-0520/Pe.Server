1b669c7ad13110af4d66836f8215573cd173e612
sk <content.type.text.net@gmail.com>
2022-08-06 05:02:03 +0900

    DatabaseResultBase

--- public_html/PeServer/Core/Database/DatabaseResultBase.php
+++ public_html/PeServer/Core/Database/DatabaseResultBase.php
@@ -9,6 +9,13 @@ namespace PeServer\Core\Database;
  */
 abstract class DatabaseResultBase
 {
+	/**
+	 * 生成。
+	 *
+	 * @param DatabaseColumn[] $columns
+	 * @param $resultCount
+	 * @phpstan-param UnsignedIntegerAlias $resultCount
+	 */
 	public function __construct(
 		public array $columns,
 		public int $resultCount
