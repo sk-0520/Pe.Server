545979e2e5546269db97234653fb0e80c00ff151
sk <content.type.text.net@gmail.com>
2022-08-06 05:00:56 +0900

    DatabaseColumn

--- public_html/PeServer/Core/Database/DatabaseColumn.php
+++ public_html/PeServer/Core/Database/DatabaseColumn.php
@@ -9,12 +9,26 @@ use PeServer\Core\ArrayUtility;
 use PeServer\Core\InitialValue;
 
 /**
- * カラム情報
+ * カラム情報。
  *
+ * @immutable
  * @see https://www.php.net/manual/pdostatement.getcolumnmeta.php
  */
 class DatabaseColumn
 {
+	/**
+	 * 生成。
+	 *
+	 * @param $name カラム名(name)。
+	 * @param $length カラム長(len)。
+	 * @param $precision 数値精度(precision)。
+	 * @param $table テーブル名(table)。
+	 * @param $nativeType PHP型(native_type)。
+	 * @param $driverType SQL型(driver:decl_type)。
+	 * @param $pdoType PDO型(pdo_type)。
+	 * @phpstan-param -1|PDO::PARAM_* $pdoType
+	 * @param array<mixed> $flags (flags)。
+	 */
 	public function __construct(
 		public string $name,
 		public int $length,
@@ -27,11 +41,17 @@ class DatabaseColumn
 	) {
 	}
 
+	/**
+	 * `PDOStatement::getColumnMeta` で取得した配列から `DatabaseColumn` の生成
+	 *
+	 * @param array<string,mixed> $meta
+	 * @return self
+	 */
 	public static function create(array $meta): self
 	{
 		return new DatabaseColumn(
 			ArrayUtility::getOr($meta, 'name', InitialValue::EMPTY_STRING),
-			ArrayUtility::getOr($meta, 'len', 0),
+			ArrayUtility::getOr($meta, 'len', -1),
 			ArrayUtility::getOr($meta, 'precision', 0),
 			ArrayUtility::getOr($meta, 'table', InitialValue::EMPTY_STRING),
 			ArrayUtility::getOr($meta, 'native_type', InitialValue::EMPTY_STRING),
