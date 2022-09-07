06a6cf122bf0245166f98c6aa84fdb0181d03c7b
sk <content.type.text.net@gmail.com>
2022-09-07 22:00:08 +0900

    ほんまもう

---
+++ dev/oreore/deploy/1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ue
+
+. ./env.sh
+
+curl -X POST \
+	-H "X-API-KEY: ${DEPLOY_API_KEY}" \
+	-H "X-SECRET-KEY: ${DEPLOY_API_SEC}" \
+	${URL_BASE}/startup
+
+
+
---
+++ dev/oreore/deploy/env.sh
@@ -0,0 +1,4 @@
+export DEPLOY_API_KEY='0I^IMb~Vw_U7igAq84c9VnBdQ-1vp58[656dLhsv^nClltAsx9SiMv^-mLoDospb'
+export DEPLOY_API_SEC='x9iZOII-taFi-J|E1fTQaxg5!z346[p1JlgWrUamgvfwzBPDEYxXpNhdshj97a]NsukDAsxrr1s|e6yIFm_O.l6KmzAz^PH-t45zaU76!djSrLXD8zB8C_IQ4yRdbMKr-2Lwfp,0uHBeeTn2QeDz2C52Nkd9nP7O[gu]vWJV-z0[tIT^ozkLUsEybGzX^kvWA.UbILmMi0i8s|ZH5XL^69e6DhrUiuVCN[X4jnCRSYleFc,JxQQKdyMPdVL1,mKz'
+
+export URL_BASE=http://localhost/api/administrator/deploy
