3deb4c6801824d53c28bb34e054501502e20578f
sk <content.type.text.net@gmail.com>
2022-09-09 00:35:52 +0900

    もともとフィードバック・クラッシュレポートって無法地帯だしもうええんちゃうの
    > アプリケーション用のAPIってどうするのこれ、考慮してなかった・・・

---
+++ public_html/PeServer/App/Models/Domain/Api/ApplicationApi/ApplicationApiFeedbackLogic.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PeServer\App\Models\Domain\Api\ApplicationApi;
+
+use PeServer\App\Models\AuditLog;
+use PeServer\App\Models\Domain\Api\ApiLogicBase;
+use PeServer\App\Models\ResponseJson;
+use PeServer\Core\Mvc\LogicCallMode;
+use PeServer\Core\Mvc\LogicParameter;
+use PeServer\Core\Throws\NotImplementedException;
+
+class ApplicationApiFeedbackLogic extends ApiLogicBase
+{
+	public function __construct(LogicParameter $parameter)
+	{
+		parent::__construct($parameter);
+	}
+
+	#region ApiLogicBase
+
+	protected function validateImpl(LogicCallMode $callMode): void
+	{
+		//NONE
+	}
+
+	protected function executeImpl(LogicCallMode $callMode): void
+	{
+		throw new NotImplementedException();
+		// $this->setResponseJson(ResponseJson::success([
+		// 	'size' => $size,
+		// ]));
+	}
+
+	#endregion
+}
