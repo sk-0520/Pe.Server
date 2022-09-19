4465876a8362af3f5c989bcb7389b010920e01eb
sk <content.type.text.net@gmail.com>
2022-09-19 19:27:50 +0900

    TraverseUtility

--- test/PeServerTest/Core/Collections/CollectionUtilityTest.php
+++ test/PeServerTest/Core/Collections/CollectionUtilityTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace PeServerTest\Core\Collections;
 
-use PeServer\Core\Collections\CollectionUtility;
+use PeServer\Core\Collections\TraverseUtility;
 use PeServer\Core\Throws\ArgumentException;
 use PeServerTest\Data;
 use PeServerTest\TestClass;
