8d17a4ae397017eb0ae390be190162a5608ed7c2
sk <content.type.text.net@gmail.com>
2023-06-15 20:19:40 +0900

    デプロイは4MB区切りにした

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -2,7 +2,7 @@
 
 export SETTING_API_URL=https://peserver.gq/api/administrator/deploy
 export SETTING_ARCHIVE_FILE_NAME=public_html.zip
-export SETTING_SPLIT_SIZE=10MB
+export SETTING_SPLIT_SIZE=4MB
 export SETTING_LOG_LEVEL=t
 
 export SETTING_API_KEY=$1
