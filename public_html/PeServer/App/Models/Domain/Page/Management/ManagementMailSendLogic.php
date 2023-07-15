17bbe7831eefac4a7b34223f7016e3b4e7c03b60
sk <content.type.text.net@gmail.com>
2023-07-15 22:46:02 +0900

    単純送信

--- public_html/PeServer/App/Models/Domain/Page/Management/ManagementMailSendLogic.php
+++ public_html/PeServer/App/Models/Domain/Page/Management/ManagementMailSendLogic.php
@@ -5,23 +5,70 @@ declare(strict_types=1);
 namespace PeServer\App\Models\Domain\Page\Management;
 
 use PeServer\App\Models\AppConfiguration;
+use PeServer\App\Models\AppMailer;
 use PeServer\App\Models\Domain\Page\PageLogicBase;
+use PeServer\Core\Mail\EmailAddress;
+use PeServer\Core\Mail\EmailMessage;
 use PeServer\Core\Mvc\LogicCallMode;
 use PeServer\Core\Mvc\LogicParameter;
 
 class ManagementMailSendLogic extends PageLogicBase
 {
-	public function __construct(LogicParameter $parameter, private AppConfiguration $config)
+	public function __construct(LogicParameter $parameter, private AppConfiguration $config, private AppMailer $mailer)
 	{
 		parent::__construct($parameter);
 	}
 
+	protected function startup(LogicCallMode $callMode): void
+	{
+		$this->registerParameterKeys([
+			'mail_subject',
+			'mail_to',
+			'mail_body',
+			'mail_attachment',
+		], true);
+	}
+
 	protected function validateImpl(LogicCallMode $callMode): void
 	{
-		//NONE
+		if($callMode === LogicCallMode::Initialize) {
+			return;
+		}
+
+		$this->validation('mail_subject', function(string $key, string $value) {
+			$this->validator->isNotEmpty($key, $value);
+		});
+
+		$this->validation('mail_to', function(string $key, string $value) {
+			$this->validator->isEmail($key, $value);
+		});
+
+		$this->validation('mail_body', function(string $key, string $value) {
+			$this->validator->isNotEmpty($key, $value);
+		});
 	}
 
 	protected function executeImpl(LogicCallMode $callMode): void
 	{
+		if($callMode === LogicCallMode::Initialize) {
+			return;
+		}
+
+		$mailSubject =$this->getRequest('mail_subject');
+		$mailTo =$this->getRequest('mail_to');
+		$mailBody =$this->getRequest('mail_body');
+
+		$this->mailer->subject = $mailSubject;
+
+		$this->mailer->toAddresses = [
+			new EmailAddress($mailTo),
+		];
+
+		$this->mailer->setMessage(new EmailMessage(
+			text: $mailBody
+		));
+
+
+		$this->mailer->send();
 	}
 }
