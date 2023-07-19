493ecb7c2c62588e3da52fcfcea98f3ac02d08b8
sk <content.type.text.net@gmail.com>
2023-07-19 20:05:32 +0900

    enum に変更

---
+++ public_html/PeServer/Core/Image/HorizontalAlignment.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PeServer\Core\Image;
+
+/**
+ * 水平方向 配置。
+ */
+enum HorizontalAlignment
+{
+	/** 左。 */
+	case Left;
+	/** 中央。 */
+	case Center;
+	/** 右。 */
+	case Right;
+}
---
+++ public_html/PeServer/Core/Image/VerticalAlignment.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PeServer\Core\Image;
+
+/**
+ * 垂直方向 配置。
+ */
+enum VerticalAlignment
+{
+	/** 上。 */
+	case Top;
+	/** 中央。 */
+	case Center;
+	/** 下。 */
+	case Bottom;
+}
