9c4227fce824288468033dfe3777c1b0d3436f44
sk <content.type.text.net@gmail.com>
2023-09-09 23:14:28 +0900

    未実装をログ出力

--- public_html/PeServer/App/Models/Domain/AppEraser.php
+++ public_html/PeServer/App/Models/Domain/AppEraser.php
@@ -5,18 +5,21 @@ declare(strict_types=1);
 namespace PeServer\App\Models\Domain;
 
 use PeServer\App\Models\AppConfiguration;
+use PeServer\Core\Log\ILogger;
+use PeServer\Core\Log\ILoggerFactory;
 
 class AppEraser
 {
 	public function __construct(
-		private AppConfiguration $config
+		private AppConfiguration $config,
+		private ILogger $logger
 	) {
-		//NOP
 	}
 
 	#region function
 
 	public function execute() : void {
+		$this->logger->info('未実装');
 	}
 
 	#endregion
