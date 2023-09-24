1789df80caf5c237c381639f3701eee7ff0fb992
sk <content.type.text.net@gmail.com>
2023-09-24 15:55:09 +0900

    セッションとかCSRFどこでどうやってんのかもう忘れたな！

--- public_html/PeServer/App/Models/SessionKey.php
+++ public_html/PeServer/App/Models/SessionKey.php
@@ -8,7 +8,21 @@ abstract class SessionKey
 {
 	#region function
 
+	/**
+	 * アカウント情報。
+	 *
+	 * ログイン済みの状態。
+	 */
 	public const ACCOUNT = 'account';
+	/**
+	 * 一時的なセッション。
+	 *
+	 * 以下の画面でCSRFを使用するためにトークンが突っ込まれるイメージ
+	 * * ログイン画面
+	 * * パスワード再発行
+	 * * 新規登録
+	 */
+	public const TEMPORARY = 'temporary';
 
 	#endregion
 }
