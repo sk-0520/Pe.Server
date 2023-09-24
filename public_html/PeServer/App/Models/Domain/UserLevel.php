f9c13193a09031516773022abfc4522f8e74e2dc
sk <content.type.text.net@gmail.com>
2023-09-24 21:58:12 +0900

    破棄: Match expression does not handle remaining value

--- public_html/PeServer/App/Models/Domain/UserLevel.php
+++ public_html/PeServer/App/Models/Domain/UserLevel.php
@@ -6,6 +6,7 @@ namespace PeServer\App\Models\Domain;
 
 use PeServer\Core\I18n;
 use PeServer\Core\Text;
+use PeServer\Core\Throws\NotImplementedException;
 
 abstract class UserLevel
 {
@@ -20,6 +21,7 @@ abstract class UserLevel
 			self::USER => I18n::message('enum/user_level/user'),
 			self::SETUP => I18n::message('enum/user_level/setup'),
 			self::ADMINISTRATOR => I18n::message('enum/user_level/administrator'),
+			default => throw new NotImplementedException()
 		};
 	}
 }
