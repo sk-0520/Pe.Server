8c3c6579af6a704486229a4c0f5a14023405b893
sk <content.type.text.net@gmail.com>
2023-10-18 21:39:49 +0900

    BinaryContent 継承

--- public_html/PeServer/Core/Http/Client/StaticContentBase.php
+++ public_html/PeServer/Core/Http/Client/StaticContentBase.php
@@ -5,13 +5,6 @@ declare(strict_types=1);
 namespace PeServer\Core\Http\Client;
 
 use PeServer\Core\Binary;
-use PeServer\Core\Collections\Dictionary;
-use PeServer\Core\Encoding;
-use PeServer\Core\Http\HttpHeader;
-use PeServer\Core\Mime;
-use PeServer\Core\Serialization\JsonSerializer;
-use PeServer\Core\Text;
-use PeServer\Core\TypeUtility;
 
 /**
  * 構築時点でもう粗方確定している本文データ。
