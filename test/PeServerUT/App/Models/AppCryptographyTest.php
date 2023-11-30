b3e6f3424ee045cd101f89e7038af1a5d29974f0
sk <content.type.text.net@gmail.com>
2023-12-01 00:00:52 +0900

    fnv132 の処理たぶんなんかミスってるけどデータ生きてるしこれもうどうにもできん

--- test/PeServerUT/App/Models/AppCryptographyTest.php
+++ test/PeServerUT/App/Models/AppCryptographyTest.php
@@ -5,10 +5,28 @@ declare(strict_types=1);
 namespace PeServerUT\App\Models;
 
 use PeServer\App\Models\AppCryptography;
+use PeServerTest\Data;
 use PeServerTest\TestClass;
 
 class AppCryptographyTest extends TestClass
 {
+	public function test_enc_dec()
+	{
+		/** @var AppCryptography */
+		$appCryptography = $this->container()->new(AppCryptography::class);
+
+		$tests = [
+			'a',
+			'あ',
+			'💩',
+		];
+		foreach ($tests as $test) {
+			$data = $appCryptography->encrypt($test);
+			$actual = $appCryptography->decrypt($data);
+			$this->assertSame($test, $actual);
+		}
+	}
+
 	public function test_token()
 	{
 		/** @var AppCryptography */
@@ -25,4 +43,18 @@ class AppCryptographyTest extends TestClass
 			$this->assertSame($test, $actual);
 		}
 	}
+
+	// public function test_convertMarker()
+	// {
+	// 	/** @var AppCryptography */
+	// 	$appCryptography = $this->container()->new(AppCryptography::class);
+
+	// 	$tests = [
+	// 		new Data(0x439c2f4b, 'abc'),
+	// 	];
+	// 	foreach ($tests as $test) {
+	// 		$actual = $appCryptography->toMark(...$test->args);
+	// 		$this->assertSame($test->expected, $actual);
+	// 	}
+	// }
 }
