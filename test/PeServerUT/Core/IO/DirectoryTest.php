764f5c9319c77177a067db6e0332b3c697bfde3b
sk <content.type.text.net@gmail.com>
2023-12-18 23:42:47 +0900

    test_exists

--- test/PeServerUT/Core/IO/DirectoryTest.php
+++ test/PeServerUT/Core/IO/DirectoryTest.php
@@ -6,6 +6,7 @@ namespace PeServerUT\Core\IO;
 
 use PeServerTest\TestClass;
 use PeServer\Core\IO\Directory;
+use PeServer\Core\IO\File;
 use PeServer\Core\IO\IOUtility;
 use PeServer\Core\IO\Path;
 
@@ -39,5 +40,22 @@ class DirectoryTest extends TestClass
 		$this->assertTrue(IOUtility::exists($testDir->path));
 		$this->assertFalse(IOUtility::exists($path));
 		$this->assertFalse(Directory::createParentDirectoryIfNotExists($path));
+	}
+
+	public function test_exists()
+	{
+		$testDir = $this->testDir();
+		$path = $testDir->newPath(__FUNCTION__);
+
+		$this->assertFalse(Directory::exists($path));
+
+		Directory::createDirectory($path);
+		$this->assertTrue(Directory::exists($path));
+
+		$path2 = $testDir->newPath(__FUNCTION__ . '-2');
+		$this->assertFalse(Directory::exists($path2));
+		File::createEmptyFileIfNotExists($path2);
+		$this->assertTrue(File::exists($path2));
+		$this->assertFalse(Directory::exists($path2));
 	}
 }
