072bf4ac65fb5034b2e8d89f7e5671cfd185c980
sk <content.type.text.net@gmail.com>
2024-01-09 20:03:02 +0900

    最低限の準備ですん

--- public_html/PeServer/Core/TemplateEngine/TemplateOptions.php
+++ public_html/PeServer/Core/TemplateEngine/TemplateOptions.php
@@ -9,15 +9,17 @@ use PeServer\Core\Collection\Vector;
 readonly class TemplateOptions
 {
 	/**
-	 *
+	 * 生成。
 	 * @param string[] $templateDirectories 読み込み対象ディレクトリパス一覧(先頭が優先される)。
 	 * @param string $compileDirectory
 	 * @param string $cacheDirectory
+	 * @param ITemplateVariableFilter $baseVariableFilter
 	 */
 	public function __construct(
-		public array $templateDirectories = [],
+		public array $templateDirectories,
 		public string $compileDirectory,
 		public string $cacheDirectory,
+		public ITemplateVariableFilter $baseVariableFilter,
 	) {
 		//NOP
 	}
