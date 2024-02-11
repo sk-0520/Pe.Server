e3cc0fe0033548b76085ab68c534590537472825
sk <content.type.text.net@gmail.com>
2024-02-11 17:46:40 +0900

    ん、ん～、先に圧縮処理かなぁ

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -ue
+cd "$(cd "$(dirname "${0}")"; pwd)"
 
 export SETTING_API_URL=https://peserver.site/api/administrator/deploy
-export SETTING_ARCHIVE_FILE_NAME=public_html.zip
+export SETTING_ARCHIVE_FILE_NAME=package.zip
 export SETTING_SPLIT_SIZE=4MB
 export SETTING_LOG_LEVEL=t
 
 export SETTING_API_KEY=$1
 export SETTING_API_SECRET=$2
 
-$(cd $(dirname $0); pwd)/deploy-core.sh
+./deploy-core.sh
