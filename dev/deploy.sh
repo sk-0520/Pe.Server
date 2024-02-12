397b521af70163a46a92540f1ca915a321e46d0c
sk <content.type.text.net@gmail.com>
2024-02-12 20:38:22 +0900

    💩

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -2,11 +2,11 @@
 cd "$(cd "$(dirname "${0}")"; pwd)"
 
 export SETTING_API_URL=https://peserver.site/api/administrator/deploy
-export SETTING_ARCHIVE_FILE_NAME=package.zip
 export SETTING_SPLIT_SIZE=4MB
 export SETTING_LOG_LEVEL=t
 
-export SETTING_API_KEY=$1
-export SETTING_API_SECRET=$2
+export SETTING_ARCHIVE_FILE_NAME=$1
+export SETTING_API_KEY=$2
+export SETTING_API_SECRET=$3
 
 ./deploy-core.sh
