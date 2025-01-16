223039ebc161fb76f67ca672a809b8af97121392
sk <content.type.text.net@gmail.com>
2025-01-16 18:08:43 +0900

    たぶんこれで切り替えできたはず

--- PeServer/App/Models/AppErrorHandler.php
+++ PeServer/App/Models/AppErrorHandler.php
@@ -65,33 +65,6 @@ final class AppErrorHandler extends HttpErrorHandler
 	#region function
 
 	/**
-	 * 検出できるソースファイル内容をすべて取得。
-	 *
-	 * @param string $file
-	 * @param Throwable|null $throwable
-	 * @return array<string,string>
-	 */
-	private function getFileContents(string $file, ?Throwable $throwable): array
-	{
-		$files = [
-			"$file" => File::readContent($file)->raw,
-		];
-
-		if ($throwable !== null) {
-			foreach ($throwable->getTrace() as $item) {
-				if (isset($item['file'])) {
-					$f = $item['file'];
-					if (!isset($files[$f])) {
-						$files[$f] = File::readContent($f)->raw;
-					}
-				}
-			}
-		}
-
-		return $files;
-	}
-
-	/**
 	 * 抑制エラーコード指定。
 	 *
 	 * @return HttpStatus[]
@@ -118,8 +91,24 @@ final class AppErrorHandler extends HttpErrorHandler
 		return $status;
 	}
 
-	protected function viewError(int $errorNumber, string $message, string $file, int $lineNumber, ?Throwable $throwable): void
+	#endregion
+
+	#region HttpErrorHandler
+
+	protected function registerImpl(): void
 	{
+		$isProduction = $this->environment->isProduction();
+		if ($isProduction) {
+			$this->registerDefault();
+		} else {
+			parent::registerImpl();
+		}
+	}
+
+	protected function catchError(int $errorNumber, string $message, string $file, int $lineNumber, ?Throwable $throwable): void
+	{
+		$isJson = Text::startsWith($this->requestPath->full, 'api/', true) || Text::startsWith($this->requestPath->full, 'ajax/', true);
+
 		$response = new HttpResponse();
 		$response->status = $this->getHttpStatus($throwable);
 
@@ -129,7 +118,6 @@ final class AppErrorHandler extends HttpErrorHandler
 			'file' => $file,
 			'line_number' => $lineNumber,
 			'throwable' => $throwable,
-			'cache' => $this->getFileContents($file, $throwable)
 		];
 
 		$isSuppressionStatus = false;
@@ -144,113 +132,44 @@ final class AppErrorHandler extends HttpErrorHandler
 			$this->logger->error($values);
 		}
 
-		$options = new TemplateOptions(
-			Path::combine(__DIR__, '..'),
-			'template',
-			$this->programContext,
-			UrlHelper::none(),
-			$this->webSecurity,
-			Path::combine(Directory::getTemporaryDirectory(), 'PeServer-Core')
-		);
-		$template = $this->templateFactory->createTemplate($options);
-
-		$response->content = $template->build('error-display.tpl', new TemplateParameter($response->status, $values, []));
-
-		$printer = $this->responsePrinterFactory->createResponsePrinter(HttpRequest::none(), $response);
-
-		$printer->execute();
-	}
-
-	#endregion
-
-	#region HttpErrorHandler
-
-	protected function registerImpl(): void
-	{
-		$isProduction = $this->environment->isProduction();
-		if($isProduction) {
-			$this->registerDefault();
-		} else {
-			parent::register();
-		}
-	}
-
-	protected function catchError(int $errorNumber, string $message, string $file, int $lineNumber, ?Throwable $throwable): void
-	{
-		$next = true;
-
-		$isJson = Text::startsWith($this->requestPath->full, 'api/', true) || Text::startsWith($this->requestPath->full, 'ajax/', true);
 		if ($isJson) {
-			$next = false;
-		}
-
-		// デバッグ環境で本番用エラー検証用
-		//$next = false;
-
-		if (!$next) {
-			$response = new HttpResponse();
-			$response->status = $this->getHttpStatus($throwable);
-
-			$values = [
-				'error_number' => $errorNumber,
-				'message' => $message,
-				'file' => $file,
-				'line_number' => $lineNumber,
-				'throwable' => $throwable,
-			];
-
-			$isSuppressionStatus = false;
-			foreach ($this->getSuppressionStatusList() as $suppressionStatus) {
-				if ($response->status === $suppressionStatus) {
-					$isSuppressionStatus = true;
-					$this->logger->info('HTTP {0}: {1}', $suppressionStatus->value, $suppressionStatus->name);
-					break;
-				}
-			}
-			if (!$isSuppressionStatus) {
-				$this->logger->error($values);
-			}
-
-			if ($isJson) {
+			if(!$this->environment->isProduction()) {
 				unset($values['error_number']);
 				unset($values['message']);
 				unset($values['throwable']);
-
-				$responseJson = ResponseJson::error(
-					$message,
-					strval($errorNumber),
-					$values
-				);
-
-				$json = [
-					'data' => $responseJson->data,
-					'error' => $responseJson->error,
-				];
-
-				$response->header->addValue(ContentType::NAME, Mime::JSON);
-				$response->content = $this->jsonSerializer->save($json);
-			} else {
-				$rootDir = Path::combine($this->programContext->applicationDirectory, 'App', 'Views');
-				$baseDir = Path::combine('template', 'page');
-
-				$options = new TemplateOptions(
-					$rootDir,
-					$baseDir,
-					$this->programContext,
-					$this->urlHelper,
-					$this->webSecurity,
-					Path::combine(Directory::getTemporaryDirectory(), 'PeServer-App')
-				);
-				$template = $this->templateFactory->createTemplate($options);
-				$response->content =  $template->build('error.tpl', new TemplateParameter($response->status, $values, []));
 			}
 
-			$printer = $this->responsePrinterFactory->createResponsePrinter(HttpRequest::none(), $response);
-			$printer->execute();
-			return;
+			$responseJson = ResponseJson::error(
+				$message,
+				strval($errorNumber),
+				$values
+			);
+
+			$json = [
+				'data' => $responseJson->data,
+				'error' => $responseJson->error,
+			];
+
+			$response->header->addValue(ContentType::NAME, Mime::JSON);
+			$response->content = $this->jsonSerializer->save($json);
+		} else {
+			$rootDir = Path::combine($this->programContext->applicationDirectory, 'App', 'Views');
+			$baseDir = Path::combine('template', 'page');
+
+			$options = new TemplateOptions(
+				$rootDir,
+				$baseDir,
+				$this->programContext,
+				$this->urlHelper,
+				$this->webSecurity,
+				Path::combine(Directory::getTemporaryDirectory(), 'PeServer-App')
+			);
+			$template = $this->templateFactory->createTemplate($options);
+			$response->content =  $template->build('error.tpl', new TemplateParameter($response->status, $values, []));
 		}
 
-		$this->viewError($errorNumber, $message, $file, $lineNumber, $throwable);
+		$printer = $this->responsePrinterFactory->createResponsePrinter(HttpRequest::none(), $response);
+		$printer->execute();
 	}
 
 	#endregion
