dc70977b6dcddef53ad3af2c20cd1288df82055b
sk <content.type.text.net@gmail.com>
2025-01-20 20:38:13 +0900

    get

--- PeServer/Core/Collection/Access.php
+++ PeServer/Core/Collection/Access.php
@@ -5,26 +5,89 @@ declare(strict_types=1);
 namespace PeServer\Core\Collection;
 
 use PeServer\Core\Throws\AccessKeyNotFoundException;
+use PeServer\Core\Throws\AccessValueTypeException;
+use PeServer\Core\TypeUtility;
 
 class Access
 {
 	#region function
 
 	/**
-	 * 値を取得する。
+	 * 生値を取得する。
 	 *
 	 * @param array $array
 	 * @phpstan-param array<mixed> $array
 	 * @param array-key $key
 	 * @return mixed
+	 * @throws AccessKeyNotFoundException キーが見つからない。
 	 */
-	public static function getRawValue(array $array, string|int $key): mixed
+	public static function getValue(array $array, string|int $key): mixed
 	{
 		if (array_key_exists($key, $array)) {
 			return $array[$key];
 		}
 
-		throw new AccessKeyNotFoundException($key);
+		throw new AccessKeyNotFoundException("key = $key");
+	}
+
+	/**
+	 *
+	 * @param array $array
+	 * @phpstan-param array<mixed> $array
+	 * @param string|int $key
+	 * @param array-key $key
+	 * @return int
+	 * @throws AccessKeyNotFoundException キーが見つからない。
+	 * @throws AccessValueTypeException
+	 */
+	public static function getInteger(array $array, string|int $key): int
+	{
+		$value = self::getValue($array, $key);
+		if (is_integer($value)) {
+			return $value;
+		}
+
+		throw new AccessValueTypeException("$key is " . TypeUtility::getType($value));
+	}
+
+	/**
+	 *
+	 * @param array $array
+	 * @phpstan-param array<mixed> $array
+	 * @param string|int $key
+	 * @param array-key $key
+	 * @return float
+	 * @throws AccessKeyNotFoundException キーが見つからない。
+	 * @throws AccessValueTypeException
+	 */
+	public static function getFloat(array $array, string|int $key): float
+	{
+		$value = self::getValue($array, $key);
+		if (is_float($value)) {
+			return $value;
+		}
+
+		throw new AccessValueTypeException("$key is " . TypeUtility::getType($value));
+	}
+
+	/**
+	 *
+	 * @param array $array
+	 * @phpstan-param array<mixed> $array
+	 * @param string|int $key
+	 * @param array-key $key
+	 * @return string
+	 * @throws AccessKeyNotFoundException キーが見つからない。
+	 * @throws AccessValueTypeException
+	 */
+	public static function getString(array $array, string|int $key): string
+	{
+		$value = self::getValue($array, $key);
+		if (is_string($value)) {
+			return $value;
+		}
+
+		throw new AccessValueTypeException("$key is " . TypeUtility::getType($value));
 	}
 
 	#endregion
--- test/PeServerUT/Core/Collection/AccessTest.php
+++ test/PeServerUT/Core/Collection/AccessTest.php
@@ -6,6 +6,7 @@ namespace PeServerUT\Core\Collection;
 
 use PeServer\Core\Collection\Access;
 use PeServer\Core\Throws\AccessKeyNotFoundException;
+use PeServer\Core\Throws\AccessValueTypeException;
 use PeServerTest\TestClass;
 use PHPUnit\Framework\Attributes\TestWith;
 
@@ -16,20 +17,81 @@ class AccessTest extends TestClass
 	#[TestWith([10, [10, 'a', 'key' => 'value'], 0])]
 	#[TestWith(['a', [10, 'a', 'key' => 'value'], 1])]
 	#[TestWith(['value', [10, 'a', 'key' => 'value'], 'key'])]
-	public function test_getRawValue(mixed $expected, array $array, string|int $key)
+	#[TestWith([null, [10, 'a', 'key' => 'value', 'none' => null], 'none'])]
+	public function test_getValue(mixed $expected, array $array, string|int $key)
 	{
-		$actual = Access::getRawValue($array, $key);
+		$actual = Access::getValue($array, $key);
 		$this->assertSame($expected, $actual);
 	}
 
-	public function test_getRawValue_throw(): void
+	#[TestWith([[], 0])]
+	#[TestWith([[], 'key'])]
+	public function test_getValue_throw(array $array, string|int $key): void
 	{
 		$this->expectException(AccessKeyNotFoundException::class);
-		Access::getRawValue([], 'key');
+		Access::getValue($array, $key);
 		$this->fail();
 	}
 
+	public function test_getInteger()
+	{
+		$actual = Access::getInteger([10], 0);
+		$this->assertSame(10, $actual);
+	}
 
+	public function test_getInteger_key_throw()
+	{
+		$this->expectException(AccessKeyNotFoundException::class);
+		Access::getInteger([10], 1);
+		$this->fail();
+	}
+
+	public function test_getInteger_type_throw()
+	{
+		$this->expectException(AccessValueTypeException::class);
+		Access::getInteger(['10'], 0);
+		$this->fail();
+	}
+
+	public function test_getFloat()
+	{
+		$actual = Access::getFloat([10.0], 0);
+		$this->assertSame(10.0, $actual);
+	}
+
+	public function test_getFloat_key_throw()
+	{
+		$this->expectException(AccessKeyNotFoundException::class);
+		Access::getFloat([10], 1);
+		$this->fail();
+	}
+
+	public function test_getFloat_type_throw()
+	{
+		$this->expectException(AccessValueTypeException::class);
+		Access::getFloat([10], 0);
+		$this->fail();
+	}
+
+	public function test_geString()
+	{
+		$actual = Access::getString(['str'], 0);
+		$this->assertSame('str', $actual);
+	}
+
+	public function test_getString_key_throw()
+	{
+		$this->expectException(AccessKeyNotFoundException::class);
+		Access::getString(['str'], 1);
+		$this->fail();
+	}
+
+	public function test_getString_type_throw()
+	{
+		$this->expectException(AccessValueTypeException::class);
+		Access::getString([10], 0);
+		$this->fail();
+	}
 
 	#endregion
 }
