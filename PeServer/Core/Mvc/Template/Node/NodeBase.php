b3143fce4e006275c05e796bdc5871cddf6d657c
sk <content.type.text.net@gmail.com>
2025-01-25 21:12:20 +0900

    継承関係変更

---
+++ PeServer/Core/Mvc/Template/Node/NodeBase.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PeServer\Core\Mvc\Template\Node;
+
+use PeServer\Core\Throws\NotImplementedException;
+use Stringable;
+
+readonly abstract class NodeBase implements INode
+{
+	#region INode
+
+	abstract public function toString(int $level): string;
+
+	public function __toString(): string
+	{
+		return $this->toString(0);
+	}
+
+	#endregion
+}
