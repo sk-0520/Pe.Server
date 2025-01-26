af09820332ff5c6df4ebc0340cfc09bdb86595c8
sk <content.type.text.net@gmail.com>
2025-01-26 19:10:34 +0900

    いや、やっぱつらい

---
+++ PeServer/Core/Mvc/Template/Node/Html/Attr.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PeServer\Core\Mvc\Template\Node\Html;
+
+use PeServer\Core\Mvc\Template\Node\Element;
+use PeServer\Core\Mvc\Template\Node\INode;
+use PeServer\Core\Mvc\Template\Node\Html\HTMLElement;
+
+class Attr
+{
+	#region function
+
+	/**
+	 * HTML 属性。
+	 *
+	 * @param array<string,string> $attributes
+	 * @phpstan-param array{}&HtmlTagAttributeAlias $attributes
+	 *
+	 * @return HtmlAttributes
+	 */
+	public function html(array $attributes = []): HtmlAttributes
+	{
+		return new HtmlAttributes($attributes);
+	}
+
+	#endregion
+}
