673f3cfc6d4cb4ce972f67f6313e5449c7b1dfcb
sk <content.type.text.net@gmail.com>
2025-01-27 23:27:03 +0900

    これ HTML 全部作れというのか・・・

--- PeServer/Core/Mvc/Template/Node/Html/Element/HTMLHeadElement.php
+++ PeServer/Core/Mvc/Template/Node/Html/Element/HTMLHeadElement.php
@@ -14,14 +14,14 @@ class HTMLHeadElement extends HTMLElement
 	 * 生成。
 	 *
 	 * @param HTMLHeadAttributes $attributes
-	 * @param object $props
 	 * @param INode[] $children
+	 * @param object $props
 	 */
 	public function __construct(
 		HTMLHeadAttributes $attributes = new HTMLHeadAttributes(),
 		array $children = [],
 		object $props = new stdClass()
 	) {
-		parent::__construct("head", $attributes, $children, $props, HtmlElementOptions::block());
+		parent::__construct("head", $attributes, $children, $props, HtmlElementOptions::block(false));
 	}
 }
