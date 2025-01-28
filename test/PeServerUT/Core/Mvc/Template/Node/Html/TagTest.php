900f5ec001c6da2357bb7d0888d8d26ef7d6842e
sk <content.type.text.net@gmail.com>
2025-01-28 21:22:18 +0900

    これあとクソほどあるタグ作らなあかんのか・・・

--- test/PeServerUT/Core/Mvc/Template/Node/Html/TagTest.php
+++ test/PeServerUT/Core/Mvc/Template/Node/Html/TagTest.php
@@ -15,7 +15,77 @@ class TagTest extends TestClass
 	{
 		$tag = new Tag();
 		$actual = $tag->html();
-		$this->assertSame('html', $actual->tagName);
+		$this->assertSame('<html></html>', (string)$actual);
+	}
+
+	public function test_head()
+	{
+		$tag = new Tag();
+		$actual = $tag->head();
+		$this->assertSame('<head></head>', (string)$actual);
+	}
+
+	public function test_title()
+	{
+		$tag = new Tag();
+		$actual = $tag->title();
+		$this->assertSame('<title></title>', (string)$actual);
+	}
+
+	public function test_base()
+	{
+		$tag = new Tag();
+		$actual = $tag->base();
+		$this->assertSame('<base />', (string)$actual);
+	}
+
+	public function test_link()
+	{
+		$tag = new Tag();
+		$actual = $tag->link();
+		$this->assertSame('<link />', (string)$actual);
+	}
+
+	public function test_style()
+	{
+		$tag = new Tag();
+		$actual = $tag->style();
+		$this->assertSame('<style></style>', (string)$actual);
+	}
+
+	public function test_script()
+	{
+		$tag = new Tag();
+		$actual = $tag->script();
+		$this->assertSame('<script></script>', (string)$actual);
+	}
+
+	public function test_meta()
+	{
+		$tag = new Tag();
+		$actual = $tag->meta();
+		$this->assertSame('<meta />', (string)$actual);
+	}
+
+	public function test_noscript()
+	{
+		$tag = new Tag();
+		$actual = $tag->noscript();
+		$this->assertSame('<noscript></noscript>', (string)$actual);
+	}
+
+	public function test_template()
+	{
+		$tag = new Tag();
+		$actual = $tag->template();
+		$this->assertSame('<template></template>', (string)$actual);
+	}
+
+	public function test_body()
+	{
+		$tag = new Tag();
+		$actual = $tag->body();
+		$this->assertSame('<body></body>', (string)$actual);
 	}
 
 	#endregion
