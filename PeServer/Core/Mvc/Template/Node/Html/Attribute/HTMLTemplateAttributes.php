f93fc9a13df8cca965a082236571e3860c76302b
sk <content.type.text.net@gmail.com>
2025-01-28 18:36:39 +0900

    noscript/template

---
+++ PeServer/Core/Mvc/Template/Node/Html/Attribute/HTMLTemplateAttributes.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PeServer\Core\Mvc\Template\Node\Html\Attribute;
+
+use PeServer\Core\Mvc\Template\Node\Html\Attribute\HtmlAttributes;
+
+class HTMLTemplateAttributes extends HtmlAttributes
+{
+	/**
+	 * 生成。
+	 *
+	 * @param array<string,int|bool|string> $attributes
+	 * @phpstan-param array{
+	 * 	shadowrootmode?: "open"|"closed",
+	 * 	shadowrootclonable?: null,
+	 * 	shadowrootdelegatesfocus?: bool,
+	 * 	shadowrootclonable?: null,
+	 * }&HtmlTagAttributeAlias $attributes
+	 */
+	public function __construct(array $attributes = [])
+	{
+		parent::__construct($attributes);
+	}
+}
