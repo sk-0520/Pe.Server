9aeb0e15f96c39578a997cbed1271a821d7eeb20
sk <content.type.text.net@gmail.com>
2025-01-29 20:01:57 +0900

    cite

--- test/PeServerUT/Core/Mvc/Template/Node/Html/TagTest.php
+++ test/PeServerUT/Core/Mvc/Template/Node/Html/TagTest.php
@@ -193,5 +193,15 @@ class TagTest extends TestClass
 		$this->assertSame('<caption></caption>', (string)$actual);
 	}
 
+	public function test_cite()
+	{
+		$tag = new Tag();
+		$actual = $tag->cite();
+		$this->assertSame('<cite></cite>', (string)$actual);
+	}
+
+
+
+
 	#endregion
 }
