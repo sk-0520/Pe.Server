8a65f94aa65ddb9201a755058b8b4c07a570c7a2
sk <content.type.text.net@gmail.com>
2025-01-29 19:10:31 +0900

    aside

--- test/PeServerUT/Core/Mvc/Template/Node/Html/TagTest.php
+++ test/PeServerUT/Core/Mvc/Template/Node/Html/TagTest.php
@@ -123,5 +123,12 @@ class TagTest extends TestClass
 		$this->assertSame('<article></article>', (string)$actual);
 	}
 
+	public function test_aside()
+	{
+		$tag = new Tag();
+		$actual = $tag->aside();
+		$this->assertSame('<aside></aside>', (string)$actual);
+	}
+
 	#endregion
 }
