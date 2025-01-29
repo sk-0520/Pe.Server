7850ca7582548732191394dc618546d1f6ab1d93
sk <content.type.text.net@gmail.com>
2025-01-29 20:08:47 +0900

    col

---
+++ PeServer/Core/Mvc/Template/Node/Html/Element/HTMLTableColElement.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PeServer\Core\Mvc\Template\Node\Html\Element;
+
+use PeServer\Core\Mvc\Template\Node\Html\Attribute\HTMLTableColAttributes;
+use PeServer\Core\Mvc\Template\Node\Props;
+
+class HTMLTableColElement extends HTMLElement
+{
+	/**
+	 * 生成。
+	 *
+	 * @param HTMLTableColAttributes $attributes
+	 * @param Props $props
+	 */
+	public function __construct(
+		HTMLTableColAttributes $attributes = new HTMLTableColAttributes(),
+		Props $props = new Props()
+	) {
+		parent::__construct(
+			"col",
+			$attributes,
+			[],
+			$props,
+			HtmlElementOptions::inline(true)
+		);
+	}
+}
