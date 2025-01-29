1ee7c9e14f019da8bd5e7ecf84781873e7493f20
sk <content.type.text.net@gmail.com>
2025-01-29 20:22:09 +0900

    インターフェイスではなから進めるべきだった・・・

---
+++ PeServer/Core/Mvc/Template/Node/Html/Element/IHTMLTableColElement.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PeServer\Core\Mvc\Template\Node\Html\Element;
+
+use PeServer\Core\Mvc\Template\Node\Html\Attribute\HtmlAttributes;
+use PeServer\Core\Mvc\Template\Node\Html\Attribute\HTMLTemplateAttributes;
+use PeServer\Core\Mvc\Template\Node\INode;
+use PeServer\Core\Mvc\Template\Node\Props;
+use PeServer\Core\Mvc\Template\Node\TextNode;
+
+interface IHTMLTableColElement extends HTMLElement
+{
+	//NOP
+}
