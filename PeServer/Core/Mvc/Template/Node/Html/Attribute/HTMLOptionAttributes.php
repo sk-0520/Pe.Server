36444e231615a9d1b6c5e5af2969d9a4275fff0c
sk <content.type.text.net@gmail.com>
2025-01-30 22:39:33 +0900

    o

---
+++ PeServer/Core/Mvc/Template/Node/Html/Attribute/HTMLOptionAttributes.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PeServer\Core\Mvc\Template\Node\Html\Attribute;
+
+use PeServer\Core\Mvc\Template\Node\Html\Attribute\HtmlAttributes;
+
+class HTMLOptionAttributes extends HtmlAttributes
+{
+	/**
+	 * 生成。
+	 *
+	 * @param array<string,int|bool|string|null> $attributes
+	 * @phpstan-param array{
+	 * 	disabled?: null,
+	 * 	label?: non-empty-string,
+	 * 	selected?: non-empty-string,
+	 * 	value?: int|bool|string,
+	 * }&HtmlTagAttributeAlias $attributes
+	 */
+	public function __construct(array $attributes = [])
+	{
+		parent::__construct($attributes);
+	}
+}
