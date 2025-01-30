a29e807fbe95e340b219d820125dfd6509d7c864
sk <content.type.text.net@gmail.com>
2025-01-30 22:39:33 +0900

    o

---
+++ PeServer/Core/Mvc/Template/Node/Html/Attribute/HTMLOptGroupAttributes.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PeServer\Core\Mvc\Template\Node\Html\Attribute;
+
+use PeServer\Core\Mvc\Template\Node\Html\Attribute\HtmlAttributes;
+
+class HTMLOptGroupAttributes extends HtmlAttributes
+{
+	/**
+	 * 生成。
+	 *
+	 * @param array<string,int|bool|string|null> $attributes
+	 * @phpstan-param array{
+	 * 	disabled?: null,
+	 * 	label?: non-empty-string,
+	 * }&HtmlTagAttributeAlias $attributes
+	 */
+	public function __construct(array $attributes = [])
+	{
+		parent::__construct($attributes);
+	}
+}
