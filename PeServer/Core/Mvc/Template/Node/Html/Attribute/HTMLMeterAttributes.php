d36d16494c707b6abb3a2cd4321ed050e7f49135
sk <content.type.text.net@gmail.com>
2025-01-30 22:00:39 +0900

    m

---
+++ PeServer/Core/Mvc/Template/Node/Html/Attribute/HTMLMeterAttributes.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PeServer\Core\Mvc\Template\Node\Html\Attribute;
+
+use PeServer\Core\Mvc\Template\Node\Html\Attribute\HtmlAttributes;
+
+class HTMLMeterAttributes extends HtmlAttributes
+{
+	/**
+	 * 生成。
+	 *
+	 * @param array<string,int|bool|string|null> $attributes
+	 * @phpstan-param array{
+	 * 	value?: int|float,
+	 * 	min?: int|float,
+	 * 	max?: int|float,
+	 * 	low?: int|float,
+	 * 	high?: int|float,
+	 * 	form?: non-empty-string,
+	 * }&HtmlTagAttributeAlias $attributes
+	 */
+	public function __construct(array $attributes = [])
+	{
+		parent::__construct($attributes);
+	}
+}
