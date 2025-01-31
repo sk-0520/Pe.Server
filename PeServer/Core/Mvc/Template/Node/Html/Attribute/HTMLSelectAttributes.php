00afd3852c15d56b55d8bf831935331a9ae9ef10
sk <content.type.text.net@gmail.com>
2025-01-31 19:06:01 +0900

    s

--- PeServer/Core/Mvc/Template/Node/Html/Attribute/HTMLSelectAttributes.php
+++ PeServer/Core/Mvc/Template/Node/Html/Attribute/HTMLSelectAttributes.php
@@ -6,21 +6,19 @@ namespace PeServer\Core\Mvc\Template\Node\Html\Attribute;
 
 use PeServer\Core\Mvc\Template\Node\Html\Attribute\HtmlAttributes;
 
-class HTMLFormAttributes extends HtmlAttributes
+class HTMLSelectAttributes extends HtmlAttributes
 {
 	/**
 	 * 生成。
 	 *
 	 * @param array<string,int|bool|string|null> $attributes
 	 * @phpstan-param array{
-	 * 	accept-charset?: non-empty-string,
 	 * 	autocomplete?: AutocompleteAlias,
-	 * 	rel?: non-empty-string,
-	 * 	action?: non-empty-string,
-	 * 	enctype?: "application/x-www-form-urlencoded"|"multipart/form-data"|"text/plain",
-	 * 	method?: "post"|"get"|"dialog",
-	 * 	novalidate?: null,
-	 * 	target?: "_self"|"_blank"|"_parent"|"_top"|"_unfencedTop",
+	 * 	disabled?: null,
+	 * 	form?: non-empty-string,
+	 * 	multiple?: null,
+	 * 	required?: null,
+	 * 	size?: int,
 	 * }&HtmlTagAttributeAlias $attributes
 	 */
 	public function __construct(array $attributes = [])
