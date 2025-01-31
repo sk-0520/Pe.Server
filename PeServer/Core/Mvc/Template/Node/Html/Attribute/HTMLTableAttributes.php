c188ff1ad9ded364a72d47b6ee5d6244a128b9de
sk <content.type.text.net@gmail.com>
2025-01-31 20:19:51 +0900

    t

---
+++ PeServer/Core/Mvc/Template/Node/Html/Attribute/HTMLTableAttributes.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PeServer\Core\Mvc\Template\Node\Html\Attribute;
+
+use PeServer\Core\Mvc\Template\Node\Html\Attribute\HtmlAttributes;
+
+class HTMLTableAttributes extends HtmlAttributes
+{
+	/**
+	 * 生成。
+	 *
+	 * @param array<string,int|bool|string|null> $attributes
+	 * @phpstan-param array{
+	 * }&HtmlTagAttributeAlias $attributes
+	 */
+	public function __construct(array $attributes = [])
+	{
+		parent::__construct($attributes);
+	}
+}
