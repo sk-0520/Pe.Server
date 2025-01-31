e08a76c4f545236eb900c99c9affaf50d7e236ae
sk <content.type.text.net@gmail.com>
2025-01-31 19:06:01 +0900

    s

--- PeServer/Core/Mvc/Template/Node/Html/Attribute/HTMLSlotAttributes.php
+++ PeServer/Core/Mvc/Template/Node/Html/Attribute/HTMLSlotAttributes.php
@@ -6,21 +6,13 @@ namespace PeServer\Core\Mvc\Template\Node\Html\Attribute;
 
 use PeServer\Core\Mvc\Template\Node\Html\Attribute\HtmlAttributes;
 
-class HTMLFormAttributes extends HtmlAttributes
+class HTMLSlotAttributes extends HtmlAttributes
 {
 	/**
 	 * 生成。
 	 *
 	 * @param array<string,int|bool|string|null> $attributes
 	 * @phpstan-param array{
-	 * 	accept-charset?: non-empty-string,
-	 * 	autocomplete?: AutocompleteAlias,
-	 * 	rel?: non-empty-string,
-	 * 	action?: non-empty-string,
-	 * 	enctype?: "application/x-www-form-urlencoded"|"multipart/form-data"|"text/plain",
-	 * 	method?: "post"|"get"|"dialog",
-	 * 	novalidate?: null,
-	 * 	target?: "_self"|"_blank"|"_parent"|"_top"|"_unfencedTop",
 	 * }&HtmlTagAttributeAlias $attributes
 	 */
 	public function __construct(array $attributes = [])
