4f68952adb102f43702875f905e34465379262a9
sk <content.type.text.net@gmail.com>
2025-02-01 19:05:07 +0900

    テスト通んねぇよ

---
+++ PeServer/Core/Mvc/Template/Node/NoneContent.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PeServer\Core\Mvc\Template\Node;
+
+final class NoneContent extends Content
+{
+	/**
+	 * 生成。
+	 */
+	public function __construct()
+	{
+		//NOP
+	}
+}
