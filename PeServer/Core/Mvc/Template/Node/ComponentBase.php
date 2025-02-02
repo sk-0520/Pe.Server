6e7832d3d815ae2f9f887053e7cfc1239e678ae1
sk <content.type.text.net@gmail.com>
2025-02-02 15:02:19 +0900

    IDEちゃんが死ぬ、かなしい

--- PeServer/Core/Mvc/Template/Node/ComponentBase.php
+++ PeServer/Core/Mvc/Template/Node/ComponentBase.php
@@ -6,17 +6,28 @@ namespace PeServer\Core\Mvc\Template\Node;
 
 use PeServer\Core\Throws\NotImplementedException;
 use PeServer\Core\Mvc\Template\Node\Attributes;
+use PeServer\Core\Mvc\Template\Node\Html\Tag;
 
+/**
+ *
+ *
+ */
 abstract class ComponentBase extends NodeBase
 {
 	/**
 	 * 生成
+	 * @template TContent of Content
+	 * @template TProps of Props
 	 * @param Content $content
+	 * @phpstan-param TContent $content
 	 * @param Props $props
+	 * @phpstan-param TProps $props
+	 * @param Tag $tag
 	 */
 	public function __construct(
 		public Content $content,
-		public readonly Props $props
+		public readonly Props $props,
+		public readonly Tag $tag
 	) {
 		//NOP
 	}
@@ -24,6 +35,22 @@ abstract class ComponentBase extends NodeBase
 	#region function
 
 	abstract protected function build(): INode;
+
+	/**
+	 * 子コンポーネントの簡易生成。
+	 * @template TComponent of ComponentBase
+	 * @template TProps of Props
+	 * @param class-string<ComponentBase> $component
+	 * @phpstan-param class-string<TComponent> $component
+	 * @param Props $props
+	 * @phpstan-param TProps $props
+	 * @return ComponentBase
+	 * @phpstan-return TComponent
+	 */
+	protected function create(string $component, Props $props): self
+	{
+		return new $component($props, $this->tag);
+	}
 
 	#endregion
 
