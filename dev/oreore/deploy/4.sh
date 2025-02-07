0c42bcc0f6945b0dffdfe103b3fa2427ea6901f0
sk <content.type.text.net@gmail.com>
2025-02-07 19:08:02 +0900

    SC1091

--- dev/oreore/deploy/4.sh
+++ dev/oreore/deploy/4.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -ue
 
+# shellcheck disable=SC1091
 . ./@env.sh
 
 curl -X POST \
 	-H "X-API-KEY: ${DEPLOY_API_KEY}" \
 	-H "X-SECRET-KEY: ${DEPLOY_API_SEC}" \
-	${URL_BASE}/update
+	"${URL_BASE}/update"
 
