dc46b43a100d9ea0c8108334b8c76950a525fd0d
sk <content.type.text.net@gmail.com>
2025-02-07 19:12:44 +0900

    SC1091

--- PeServer/cron/HealthCheck.sh
+++ PeServer/cron/HealthCheck.sh
@@ -2,6 +2,7 @@
 
 cd "$(cd "$(dirname "${0}")"; pwd)"
 
+#shellcheck disable=SC1091
 . "cronrc"
 
 "${PHP}" "${APP_DIR}/App/Cli/app.php" --mode production --class "PeServer\App\Cli\HealthCheck\HealthCheckApplication" --echo "CRON!"
