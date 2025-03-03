c3f31b2829e536d6e89b3c25e685fe8f4a5d9893
sk <content.type.text.net@gmail.com>
2025-03-03 20:38:14 +0900

    はー、つら

--- test/PeServerUT/Core/Log/LoggingTest.php
+++ test/PeServerUT/Core/Log/LoggingTest.php
@@ -31,6 +31,12 @@ class LoggingTest extends TestClass
 		$this->assertSame($expected, $actual);
 	}
 
+	public function test_formatMessage_message_none_parameters()
+	{
+		$actual = $this->callStaticMethod(Logging::class, "formatMessage", ["message"]);
+		$this->assertSame("message", $actual);
+	}
+
 	public function test_formatMessage_null_message_empty_parameters()
 	{
 		$actual = $this->callStaticMethod(Logging::class, "formatMessage", [null]);
@@ -50,5 +56,103 @@ class LoggingTest extends TestClass
 		);
 	}
 
+	public function test_formatMessage_null_message_double_parameters()
+	{
+		$actual = $this->callStaticMethod(Logging::class, "formatMessage", [null, 1, "str"]);
+		$this->assertSame(
+			"array(2) {
+  [0]=>
+  int(1)
+  [1]=>
+  string(3) \"str\"
+}
+",
+			$actual
+		);
+	}
+
+	public function test_formatMessage_no_format_message_single_parameters()
+	{
+		$actual = $this->callStaticMethod(Logging::class, "formatMessage", ["message", 1]);
+		$this->assertSame("message", $actual);
+	}
+
+	public function test_formatMessage_format_message_none_parameters()
+	{
+		$actual = $this->callStaticMethod(Logging::class, "formatMessage", ["message {0}"]);
+		$this->assertSame("message {0}", $actual);
+	}
+
+	public function test_formatMessage_format_message_single_parameters_int()
+	{
+		$actual = $this->callStaticMethod(Logging::class, "formatMessage", ["message {0}", 1]);
+		$this->assertSame("message 1", $actual);
+	}
+
+	public function test_formatMessage_format_message_single_parameters_str()
+	{
+		$actual = $this->callStaticMethod(Logging::class, "formatMessage", ["message {0}", "STR"]);
+		$this->assertSame("message STR", $actual);
+	}
+
+	public function test_formatMessage_format_message_single_parameters_arr()
+	{
+		$actual = $this->callStaticMethod(Logging::class, "formatMessage", ["message {0}", [1, "b" => 2, "c" => 'C']]);
+		$this->assertSame(
+			"message array(3) {
+  [0]=>
+  int(1)
+  [\"b\"]=>
+  int(2)
+  [\"c\"]=>
+  string(1) \"C\"
+}
+",
+			$actual
+		);
+	}
+
+	public function test_formatMessage_format_message_single_parameters_obj()
+	{
+		$obj = new LocalObj();
+		$id = spl_object_id($obj);
+		$actual = $this->callStaticMethod(Logging::class, "formatMessage", ["message {0}", $obj]);
+		$this->assertSame(
+			"message object(PeServerUT\Core\Log\LocalObj)#{$id} (2) {
+  [\"str\"]=>
+  string(3) \"STR\"
+  [\"number\"]=>
+  int(123)
+}
+",
+			$actual
+		);
+	}
+
+	public function test_formatMessage_format_message_obj_none_parameters()
+	{
+		$obj = new LocalObj();
+		$id = spl_object_id($obj);
+		$actual = $this->callStaticMethod(Logging::class, "formatMessage", [$obj]);
+		$this->assertSame(
+			"object(PeServerUT\Core\Log\LocalObj)#{$id} (2) {
+  [\"str\"]=>
+  string(3) \"STR\"
+  [\"number\"]=>
+  int(123)
+}
+",
+			$actual
+		);
+	}
+
 	#endregion
+}
+
+class LocalObj
+{
+	public string $str = "STR";
+	public int $number = 123;
+
+	public function method(): void {}
 }
