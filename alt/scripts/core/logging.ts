65f7f08805fc8782003a2d307f7a1b1b6c525c45
sk <content.type.text.net@gmail.com>
2025-03-28 01:02:19 +0900

    ログの any 許容

--- alt/scripts/core/logging.ts
+++ alt/scripts/core/logging.ts
@@ -1,8 +1,13 @@
 export interface Logger {
+	// biome-ignore lint/suspicious/noExplicitAny: console.trace
 	trace(message?: any, ...optionalParams: any[]): void;
+	// biome-ignore lint/suspicious/noExplicitAny: console.debug
 	debug(message?: any, ...optionalParams: any[]): void;
+	// biome-ignore lint/suspicious/noExplicitAny: console.info
 	info(message?: any, ...optionalParams: any[]): void;
+	// biome-ignore lint/suspicious/noExplicitAny: console.warn
 	warn(message?: any, ...optionalParams: any[]): void;
+	// biome-ignore lint/suspicious/noExplicitAny: console.error
 	error(message?: any, ...optionalParams: any[]): void;
 }
 
@@ -21,18 +26,23 @@ class LoggerImpl implements Logger {
 		return `[${this._name}]`;
 	}
 
+	// biome-ignore lint/suspicious/noExplicitAny: console.trace
 	public trace(...args: any[]): void {
 		console.trace(this.name, ...args);
 	}
+	// biome-ignore lint/suspicious/noExplicitAny: console.debug
 	public debug(...args: any[]): void {
 		console.debug(this.name, ...args);
 	}
+	// biome-ignore lint/suspicious/noExplicitAny: console.info
 	public info(...args: any[]): void {
 		console.log(this.name, ...args);
 	}
+	// biome-ignore lint/suspicious/noExplicitAny: console.warn
 	public warn(...args: any[]): void {
 		console.warn(this.name, ...args);
 	}
+	// biome-ignore lint/suspicious/noExplicitAny: console.error
 	public error(...args: any[]): void {
 		console.error(this.name, ...args);
 	}
